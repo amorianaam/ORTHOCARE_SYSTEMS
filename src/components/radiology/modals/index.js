6284cbe258d821701a04b7e49e8fae1fef11c857
3MORI <168870818+amorianaam@users.noreply.github.com>
2026-09-07 06:21:47 +0300

    Delete src directory

--- src/components/radiology/modals/index.js
+++
@@ -1,5 +0,0 @@
-export { default as SharedResultPreviewModal } from './SharedResultPreviewModal';
-export { default as UploadResultModal } from './UploadResultModal';
-export { default as FilmGroupingModal } from './FilmGroupingModal';
-export { default as PatientDetailPanel } from './PatientDetailPanel';
-export { default as RecordDetailModal } from './RecordDetailModal';
